dc00107972f633ff20ce4def61f68a70dad33595
PRASETIO <75734658+PRASETIO96@users.noreply.github.com>
2024-09-27 11:08:32 +0700

    TUGAS JAVA
    PUSING BOSS

---
+++ aritmatika/ass.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+    cout<<"DAVAR PEDIA"<<cendl;
+    return 0;
+}
+
---
+++ aritmatika/luas lingkaran.cpp
@@ -0,0 +1,25 @@
+#include.<iostream>
+using namespace std;
+
+int main(){
+	const float phi = 3.14;
+	float L, r;
+	
+	//value r
+	cout<<"masukkan jari-jari lingkaran = ";
+	cin>>r;
+	
+	//proses perhitungan luas 
+	L = phi * r * r;
+	
+	//pemanis
+	cout<<"diketahui lingkaran dengan = "<<endl;
+	cout<<"phi \t= "<<phi<<endl;
+	cout<<"r \t = "<<r<<endl;
+	cout<<"===================="<<endl;
+	
+	//penampilan hasil
+	cout<<"luas lingkaran dengan jari - jari "<<r<<" cm "<<"adalah ";
+	cout<<L<<" cm2 ";
+	
+}
